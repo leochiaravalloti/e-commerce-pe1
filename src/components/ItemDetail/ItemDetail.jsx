ba99abd287b1dfe3a84b696f02e197363e60d67b
Leonardo Chiaravalloti <leonardochiaravalloti1b@gmail.com>
2023-12-27 00:33:39 -0300

    add details scss

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,12 @@
+import './itemDetail.scss'
 const ItemDetail = ({ producto }) => {
     return (
       <div className="item-detail">
-        <img className="imagen" src={producto.imagen} alt={producto.nombre} />
-        <div className="texto">
-          <h2>{producto.nombre}</h2>
-          <p>{producto.descripcion}</p>
-          <p>US${producto.precio}</p>
+        <img className="item-detail__imagen" src={producto.imagen} alt={producto.nombre} />
+        <div className="item-detail__texto">
+          <h2 className="item-detail__texto__titulo">{producto.nombre}</h2>
+          <p className="item-detail__texto__descp">{producto.descripcion}</p>
+          <p className="item-detail__texto__precio">US${producto.precio}</p>
         </div>
       </div>
     );
